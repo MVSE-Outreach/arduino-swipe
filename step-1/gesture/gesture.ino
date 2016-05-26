b1a52be1733cb19c9aa8345a3df3baba160c78fc
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-05-26 13:45:08 +0100

    Adding circuit diagrams and explanations for step 1.  On branch gh-pages  Your branch is up-to-date with 'origin/gh-pages'.
     Changes to be committed:
    	new file:   step-1/circuit-step-1.svg
    	new file:   step-1/circuit.fzz
    	modified:   step-1/gesture/gesture.ino
    	modified:   step-1/index.html

--- step-1/gesture/gesture.ino
+++ step-1/gesture/gesture.ino
@@ -1,14 +1,11 @@
 
 #define SENSOR_PIN A0
-#define LED_PIN    6
 
 // This will store the value of our swipe sensor.
 int sensorValue;
 
 void setup() {
-    // Setup our input / output pins.
-    pinMode(LED_PIN, OUTPUT);
-
+    
     // Start the serial connection with the PC.
     Serial.begin(9600);
 }
