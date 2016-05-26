62aaaf8c16949708c51abe560befcfe0589e7d9e
Ben Marshall <bm12656@my.bristol.ac.uk>
2016-05-26 16:56:41 +0100

    Progress save. Need to fix filtering to detect direction of swipe.  On branch gh-pages  Your branch is up-to-date with 'origin/gh-pages'.
     Changes to be committed:
    	new file:   step-2/gesture/gesture.ino
    	new file:   step-2/gradient/gradient.ino
    	modified:   step-2/index.html

---
+++ step-2/gradient/gradient.ino
@@ -0,0 +1,47 @@
+
+#define SENSOR_PIN A0
+#define BUFFER_SIZE 20
+
+// This will store the most recent value of our swipe sensor.
+int currentSensorValue;
+int cm2;
+
+// This one will store the last 20 values.
+int previousValues[BUFFER_SIZE];
+
+// This stores the value we will compare with.
+int previousSensorValue;
+
+// This keeps track of the newest value.
+int valueIndexAdd;
+
+// This stores how fast the sensor value is changing.
+int sensorSteepness;
+
+void setup() {
+    // Set this to zero to begin with.
+    valueIndexAdd = 0;
+
+    // Start the serial connection with the PC.
+    Serial.begin(115200);
+}
+
+void loop() {
+    
+    // read the sensor value and store it.
+    currentSensorValue = analogRead(SENSOR_PIN) - 1023/2;
+
+    // Update the new steepness value.
+    sensorSteepness = currentSensorValue - previousSensorValue;
+
+    // Move the most recent sensor measurement to the previous one.
+    previousValues[valueIndexAdd] = currentSensorValue;
+    valueIndexAdd = (valueIndexAdd+1) % BUFFER_SIZE;
+    previousSensorValue = previousValues[(valueIndexAdd+1) % BUFFER_SIZE];
+
+    // print the sensor steepness value for the plotter.
+    Serial.print(currentSensorValue);
+    Serial.print(" 600 ");
+    Serial.println(sensorSteepness);
+    delay(1);
+}
